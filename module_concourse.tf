0dad0a97fb78d319ec4c7c97843037b60423e2b4
DaShaun Carter <dashaun@dashaun.com>
2024-08-02 22:44:38 +0000

    vault still not working

--- module_concourse.tf
+++ module_concourse.tf
@@ -1,8 +1,8 @@
-# module "concourse" {
-#   depends_on                     = [module.vault_operator_init]
-#   source                         = "./modules/concourse"
-#   concourse_github_client_id     = var.concourse_github_client_id
-#   concourse_github_client_secret = var.concourse_github_client_secret
-#   vault_address                  = var.vault_address
-#   vault_client_token             = module.vault_operator_init.stdout
-# }
+module "concourse" {
+  depends_on                     = [module.vault_operator_init]
+  source                         = "./modules/concourse"
+  concourse_github_client_id     = var.concourse_github_client_id
+  concourse_github_client_secret = var.concourse_github_client_secret
+  vault_address                  = var.vault_address
+  vault_client_token             = module.vault_operator_init.stdout
+}
